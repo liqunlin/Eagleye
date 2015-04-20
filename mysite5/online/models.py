f9b356d05c6ed1493446cd952723013d0fb64593
liqunlin <liqunlin@blingstorm.com.cn>
2015-04-20 18:19:26 +0800

    test
    无用文档

--- mysite5/online/models.py
+++
@@ -1,7 +0,0 @@
-﻿from django.db import models
-
-class User(models.Model):
-	username=models.CharField(max_length=50)
-	password=models.CharField(max_length=200)
-	def __unicode__(self):
-		return self.username
